ef8274cef3e6a66fb8df778e66104cb54540ba24
danyanyam <dvbuchko@edu.hse.ru>
2022-05-17 23:37:56 +0300

    added cmake

--- linked_list/linkedList/linkedList.hpp
+++ linked_list/linkedList/linkedList.hpp
@@ -24,34 +24,34 @@ public:
     void append(int value);
     // creates new node with value and inserts it
     // in the ending of linked list O(n)
-    
+
     void insertAtBeginning(int value);
     // creates new node with value and inserts it
     // at beginning of the linked list O(1)
-    
+
     void insertAfter(Node*& before, int newValue);
     // creates new node after provided node
     // with newValue O(1)
-    
+
     void remove(size_t index);
-    
+
     Node* get(size_t index);
     // returns pointer to node at index O(n)
-    
+
     void print();
     // prints the linked list
-    
+
     linkedList();
     // size = 0, head = nullptr
-    
+
     ~linkedList();
     // removes all nodes from list
-    
+
 private:
     Node* lastNode();
     // returns pointer to the last Node
     // in list O(n)
-    
+
     Node* head;
     int size;
 };
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "linked_list/linkedList/linkedList.hpp"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    linkedList x;
+    x.append(123);
+    x.append(123);
+    x.print();
+
+    return 0;
+}
